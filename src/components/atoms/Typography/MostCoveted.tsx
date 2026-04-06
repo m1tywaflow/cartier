09033dee8c09725e2e68cecf60461d7e69aa6938
Mitya <mitywaflow@gmail.com>
2026-04-06 22:05:04 +0200

    update
    Signed-off-by: Mitya <mitywaflow@gmail.com>

--- src/components/atoms/Typography/MostCoveted.tsx
+++ src/components/atoms/Typography/MostCoveted.tsx
@@ -1,17 +1,22 @@
 import { Imperial_Script } from "next/font/google";
 import Button from "../Button/ButtonViewAll";
+
 const imperial = Imperial_Script({
   subsets: ["latin"],
   weight: "400",
 });
+
 export default function MostCoveted() {
   return (
-    <div className="flex justify-between">
+    <div className="flex flex-col md:flex-row md:justify-between items-start md:items-center gap-4">
       <div className="text-black text-4xl">
         <h1 className="font-serif ">The Most Coveted</h1>
         <h1 className={` ${imperial.className} text-6xl`}>Luxury Watches</h1>
       </div>
-      <Button href="/watches" />
+
+      <div className="w-full md:w-auto">
+        <Button href="/watches" />
+      </div>
     </div>
   );
 }
--- src/components/organisms/Section/LuxuryWatches.tsx
+++ src/components/organisms/Section/LuxuryWatches.tsx
@@ -1,12 +1,13 @@
 import MostCoveted from "@/components/atoms/Typography/MostCoveted";
 import WatchCard from "@/components/molecules/WatchCards/WatchCard";
 import { cards } from "@/components/molecules/WatchCards/watchData";
+
 export default function LuxuryWatches() {
   return (
-    <section className="w-full p-20 bg-white border-b border-black">
+    <section className="w-full p-6 md:p-10 lg:p-20 bg-white border-b border-black">
       <MostCoveted />
 
-      <div className="flex gap-2 justify-between mt-8 ">
+      <div className="flex flex-col sm:flex-row sm:flex-wrap lg:flex-nowrap gap-4 mt-8">
         {cards.map((card) => (
           <WatchCard key={card.title} {...card} />
         ))}
