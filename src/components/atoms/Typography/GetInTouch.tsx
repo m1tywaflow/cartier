291263ec02e3bce959e797fab40b581f1579392b
Mitya <mitywaflow@gmail.com>
2026-04-13 08:24:47 +0200

    update
    Signed-off-by: Mitya <mitywaflow@gmail.com>

--- src/components/atoms/Typography/GetInTouch.tsx
+++ src/components/atoms/Typography/GetInTouch.tsx
@@ -1,15 +1,13 @@
 export default function GetInTouch() {
   return (
-    <>
-      <section className="w-full p-20 bg-white border-b">
-        <div className="max-w-5xl pt-20  mx-auto text-center  text-black text-4xl font-serif">
-          <h1>
-            We are here to assist you. Whether you have a question, a request,
-            or simply wish to learn more, our team will be delighted to hear
-            from you.
-          </h1>
-        </div>
-      </section>
-    </>
+    <section className="w-full pt-20 sm:p-10 md:p-16 lg:p-20 bg-white border-b">
+      <div className="max-w-5xl mx-auto text-center text-black font-serif pt-6 sm:pt-10 md:pt-14 lg:pt-20">
+        <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-4xl leading-relaxed">
+          We are here to assist you. Whether you have a question, a request,
+          or simply wish to learn more, our team will be delighted to hear
+          from you.
+        </h1>
+      </div>
+    </section>
   );
 }
